0e7dbceaa6c192f156a3e90cfd5d8d374e6a8295
n0c3rt <25084350+n0c3rt@users.noreply.github.com>
2019-02-25 15:51:41 +0700

    ml-naive-bayes-example

---
+++ ml.py
@@ -0,0 +1,23 @@
+﻿import csv
+
+with open('pima-indians-diabetes.data.csv') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',')
+	total = 768
+	training_total = (int)(total/3)
+	print(training_total)
+	training_set = []
+	test_set = []
+	count = 0
+	for row in reader:
+		count = count + 1
+		if count < training_total:
+			training_set.append(row)
+		test_set.append(row)
+		
+result_dict ={}
+for item in training_set:
+	if item[-1] not in result_dict:
+		result_dict[item[-1]] = []
+	result_dict[item[-1]].append(item)
+
+print(result_dict['0'])
